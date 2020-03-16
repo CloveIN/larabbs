9f84d5e5b77696d7c4e8c520f0b6bee22d6e827f
CloveIN <865198377@qq.com>
2020-03-16 01:46:53 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,19 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    //随机选取一个月内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    $create_at = $faker->dateTimeThisMonth($updated_at);
+
+
     return [
         // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $create_at,
+        'updated_at' => $updated_at,
     ];
 });
